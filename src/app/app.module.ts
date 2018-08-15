a75e2391554b7618b115bc44929a253b63bf68c7
l1mb0 <robin_ronson@yahoo.com>
2018-08-15 17:15:53 -0500

    Changes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { MDBBootstrapModule } from 'angular-bootstrap-md';
 
 import { AppComponent } from './app.component';
 
