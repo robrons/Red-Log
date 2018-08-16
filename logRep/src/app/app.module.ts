153e7c7ca7c725dce96e8087c96ff57c4868ca31
fl45h <fl45h@coeits-iMac.local>
2018-08-16 14:16:47 -0500

    Installed prereq

--- logRep/src/app/app.module.ts
+++ logRep/src/app/app.module.ts
@@ -1,6 +1,8 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
+import 'hammerjs';
+
 import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
 
 import { AppComponent } from './app.component';
