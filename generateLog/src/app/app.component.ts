0a634175ff7c2e0a336ecfb04c4ed330aeb5667c
fl45h <fl45h@coeits-iMac.local>
2018-08-21 09:35:38 -0500

    Fixed File choice

--- generateLog/src/app/app.component.ts
+++ generateLog/src/app/app.component.ts
@@ -1,35 +1,27 @@
 import { Component, OnInit } from '@angular/core';
-import {FormControl, Validators} from '@angular/forms';
+import { FormControl, Validators } from '@angular/forms';
 import { RetrjsnService } from './retrjsn.service';
-
-
-export interface Animal {
-  name: string;
-  sound: string;
-};
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent implements OnInit {
 
+export class AppComponent implements OnInit {
+  conformaceResults: string[];
   title = 'Conformance Results';
   animalControl = new FormControl('', [Validators.required]);
-  animals: Animal[] = [
-    {name: 'Dog', sound: 'Woof!'},
-    {name: 'Cat', sound: 'Meow!'},
-    {name: 'Cow', sound: 'Moo!'},
-    {name: 'Fox', sound: 'Wa-pa-pa-pa-pa-pa-pow!'},
-  ];
 
   constructor(
-    private retrjsnService : RetrjsnService;
-) { }
+    private retrjsnService: RetrjsnService;
+  ) { }
 
   ngOnInit() {
     this.retrjsnService.getJSON().subscribe(data => {
-      console.log(data);
-  });  }
+      if (typeof Object.keys(data) !== 'undefined' && Object.keys(data).length > 0) {
+        this.conformaceResults = Object.keys(data);
+    }
+    });
+  }
 }
