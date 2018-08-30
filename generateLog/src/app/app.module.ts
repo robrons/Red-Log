128f7697bcfb950295c883b3f91f5a665f09763d
fl45h <fl45h@coeits-iMac.local>
2018-08-30 02:11:48 -0500

    Changes

--- generateLog/src/app/app.module.ts
+++ generateLog/src/app/app.module.ts
@@ -16,12 +16,14 @@ import { AssertTableComponent } from './assert-table/assert-table.component';
 import { ChartsModule } from 'ng2-charts';
 import { StoreModule } from '@ngrx/store';
 import { simpleReducer } from './simple.reducer';
+import { AboutComponent } from './about/about.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     UpperviewComponent,
     AssertTableComponent,
+    AboutComponent,
   ],
   imports: [
     HttpClientModule,
